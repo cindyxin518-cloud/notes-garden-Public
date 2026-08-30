471ef629145f4ec1eb3c56318aee19fb635a358c
Cindy Xin <cindyxin@MacBookPro.home>
2026-08-30 23:05:57 +0200

    Purge service worker cache to enforce Screenshot 2 layout

--- metronome/sw.js
+++ metronome/sw.js
@@ -1,47 +1,16 @@
-/**
- * Service Worker - 离线缓存支持，即使户外跑步无手机信号也能秒开
- */
-
-const CACHE_NAME = 'cadence-metronome-v3';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './css/style.css',
-  './js/audio.js',
-  './js/tap.js',
-  './js/calculator.js',
-  './js/app.js',
-  './manifest.webmanifest'
-];
-
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (e) => {
   e.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
+      return Promise.all(keys.map((k) => caches.delete(k)));
     })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cachedResponse) => {
-      return cachedResponse || fetch(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
